872f2da2b74619be4c914bdc225b00de20ee4293
Anurag Shukla <tachyonanurag@gmail.com>
2022-10-03 13:42:58 +0530

    added search system

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,25 @@
 const main = document.getElementById("main")
+const searchData={
+	dateInp:false,
+	authorInp:false,
+	date:'10/2022',
+	author:'anurag',
+	tags:{}
+}
+let searchActive=false
+const closeIcon = `
+
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+</svg>
+
+`
+const searchIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+							<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+						</svg>
+`
+
 const blog = () => {
 	return `
 <div class="blog">
@@ -20,16 +41,6 @@ const blog = () => {
 					</div>
 					<div class="flex tags">
 						tags: 
-								<span>#hello</span>
-						<span>#anurag</span>
-						<span>#shukla</span>
-						<span>#world</span>
-						<span>#latest</span>
-						<span>#hello</span>
-						<span>#anurag</span>
-						<span>#shukla</span>
-						<span>#world</span>
-						<span>#latest</span>
 						<span>#hello</span>
 						<span>#anurag</span>
 						<span>#shukla</span>
@@ -40,8 +51,78 @@ const blog = () => {
 			</div>
 	`
 }
-const addBlogs = (n) =>{
+const addBlogs = n =>{
 	for(let i=0;i<n;i++)
 		main.innerHTML+=blog()
 }
-onload=addBlogs(10)
+const getBlock = id => {
+	return document.getElementById(id)
+}
+const toggleElem = id =>{
+	getBlock(id).classList.toggle('hidden')
+	searchData[id]=!searchData[id]
+	updatePublished()
+}
+const clearTag = e => {
+	delete searchData.tags[e.id]
+	e.parentNode.remove()
+	updatePublished()
+}
+const toggleSearch = (e) => {
+	getBlock('search').classList.toggle('hidden')
+	searchActive=!searchActive
+	e.innerHTML=searchActive ? closeIcon : searchIcon
+}
+const updatePublished = () => {
+	if(searchData.dateInp || searchData.authorInp){
+		getBlock('publishData').innerHTML=`Published ${searchData.dateInp ? `in <span class="date" id="date">${searchData.date}</span>`:''} ${searchData.authorInp ? `by <span class="name" id="authorName">@${searchData.author}</span>`:''}`
+	}else{
+		getBlock('publishData').innerHTML=''
+	}
+	if(Object.keys(searchData.tags).length==0){
+		getBlock("tags").classList.add('hidden')
+
+	}else{
+		getBlock("tags").classList.remove('hidden')
+	}
+}
+
+onload=()=>{
+	addBlogs(10)
+	getBlock("inp").addEventListener('keydown', (e)=>{
+		if(e.keyCode == 13) {
+			e.preventDefault()
+			const tag = e.target.value
+			e.target.value=''
+			if(tag.length!=0 && tag!="" && tag!=null && tag!=undefined ){
+				getBlock('tags').innerHTML+=`<span  class="searchItem">#${tag}<span class="close" onclick="clearTag(this)" id=${tag}>x</span></span>` 
+				searchData.tags[tag]=tag
+				updatePublished()
+			}
+		}
+	})
+	getBlock("authorInp").addEventListener('keydown', (e)=>{
+		if(e.keyCode == 13) {
+			e.preventDefault()
+			const tag = e.target.value
+			e.target.value=''
+			if(tag.length!=0 && tag!="" && tag!=null && tag!=undefined ){
+				searchData.author=tag
+				updatePublished()
+				getBlock('authorName').innerText=`@${tag}`
+			}
+		}
+	})
+	getBlock("dateInp").addEventListener('change', (e)=>{
+		e.preventDefault()
+		const val = e.target.value.split('-')
+		const tag = val[1]+'/'+val[0]
+		if(tag.length!=0 && tag!="" && tag!=null && tag!=undefined ){
+			searchData.date=tag
+			updatePublished()
+			getBlock('date').innerText=`${tag}`
+				}
+	})
+
+updatePublished()
+}
