4908668c2cb2ca8d340ff5f977f36ae75e4b985e
Anurag Shukla <tachyonanurag@gmail.com>
2022-10-08 12:33:19 +0530

    .

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const http = require('http').Server(app)
 const port = process.env.PORT || 3000
 var escapeForMongo = require('mongo-escape').escape;
 const url = process.env.DATABASE_URL
-const {MongoClient, ObjectId} = require('mongodb')
-
+const {MongoClient, ObjectId, pretty} = require('mongodb')
 app.use(express.json())
 app.use(express.urlencoded({
 	extended:true
@@ -18,14 +17,20 @@ app.get('/', (req,res)=>{
 })
 
 app.post('/search',async (req, res)=>{
-	const {searchData} = req.body
-	const data = await search(searchData)
-	res.json(data)
+	const {searchData, next} = req.body
+	const data = await getPosts({item:searchData,next, search:true })
+	console.log(data)
+	if(data.len!=0){
+		res.json({status:true, ...data })
+		return
+	}
+	res.json({status:false, ...data })
 })
 
 app.post('/blogs', async (req, res)=>{
 	const {next, fwd}=req.body
 	const {items, next:cursor, ttl, len} = await getPosts({next, fwd})
+	console.log(items)
 	if(len!=0){
 		res.json({status:true, items,next:cursor, ttl })
 		return
@@ -60,6 +65,74 @@ app.post('/add-new-post',async (req, res)=>{
 	catch(err){ console.error(err); }
 	finally{ client.close(); } 
 })
+
+async function getPosts({next=null, fwd=true, search=false, item={}}){
+	let client, db
+	let query={}
+	let page1=(next===null)
+	if(!search){
+		if(!page1){
+			let obj = {}
+			let key=(fwd ? "$lt" : "$gte")
+			obj._id={}
+			obj._id[key]=ObjectId(next)
+			query=obj
+		}
+	}else{
+		query["$and"]=[]
+		const {dateInp, authorInp, date, author,exact, tags } = item
+		if(dateInp){
+			let obj = {}
+			obj.month=date
+			query["$and"].push(obj)
+		}
+		if(authorInp){
+			let obj = {}
+			obj.author=author
+			query["$and"].push(obj)
+		}
+		if(tags.length!=0){
+			let obj = {}
+			obj.tags=exact ? {$all:tags} : {$in:tags}
+			query["$and"].push(obj)
+		}
+		if(!page1){
+			let obj = {}
+			let key=(fwd ? "$lt" : "$gte")
+			obj._id={}
+			obj._id[key]=ObjectId(next)
+			query["$and"].push(obj)
+		}
+	}
+	query=escapeForMongo(query)
+
+	try{
+		//	console.log(JSON.stringify(query, null, 4))
+		client =await MongoClient.connect(url, {useNewUrlParser: true})
+		db = client.db("mydb")
+		let dCollection = db.collection('blogs')
+		const ttl = await dCollection.countDocuments()
+		let items, nextCursor
+		items = await dCollection.find({
+			...query
+		}).sort({
+			_id: -1
+		}).limit(20).toArray();
+		const len = items.length
+		if(len==0){
+			return ({items:[],next, ttl , len})
+		}
+		nextCursor =items.length!=0? items[items.length - 1]._id:null
+		return {items, next:nextCursor, ttl, len}
+	}
+	catch(err){ 
+		console.log(err)
+	}
+	finally{ 
+		client.close()
+	}
+}
+
 /*
  *
  *async function getAll(){
@@ -81,77 +154,38 @@ app.post('/add-new-post',async (req, res)=>{
  *}
  *
  */
-async function getPosts({next, fwd}){
-	let client, db
-	let first=false
-	if(next==null){
-		first=true
-	}
-	try{
-		client =await MongoClient.connect(url, {useNewUrlParser: true})
-		db = client.db("mydb")
-		let dCollection = db.collection('blogs')
-		const ttl = await dCollection.countDocuments()
-		let items, nextCursor
-		if(first){
-			items = await dCollection.find({}).sort({
-				_id: -1
-			}).limit(20).toArray()
-		}else{
-			let query = {}
-			let key=(fwd ? "$lt" : "$gte")
-			query[key]=new ObjectId(next)
-			console.log(query)
-			items = await dCollection.find({
-				_id: query
-			}).sort({
-				_id: -1
-			}).limit(20).toArray();
 
-		}
-		const len = items.length
-		if(len==0){
-			return ({items:[],next, ttl , len})
-		}
-		nextCursor =items.length!=0? items[items.length - 1]._id:null
-		return {items, next:nextCursor, ttl, len}
-	}
-	catch(err){ 
-		console.log(err)
-	}
-	finally{ 
-		client.close()
-	}
-}
 
-async function search(item){
-	const {dateInp, authorInp, date, author,exact, tags } = item
-	let query = {}
-	if(dateInp){
-		query.month=date
-	}
-	if(authorInp){
-		query.author=author
-	}
-	if(tags.length!=0)
-		query.tags=exact ? {$all:tags} : {$in:tags}
-	query=escapeForMongo(query)
-	let client, db
-	try{
-		client =await MongoClient.connect(url, {useNewUrlParser: true})
-		db = client.db("mydb")
-		let dCollection = db.collection('blogs')
-		const data = await dCollection.find(query).toArray()
-		return data
-	}
-	catch(err){ 
-		console.log(err)
-	}
-	finally{ 
-		client.close()
-	} 
-}
-
+/*
+ *async function search(item){
+ *    const {dateInp, authorInp, date, author,exact, tags } = item
+ *    let query = {}
+ *    if(dateInp){
+ *        query.month=date
+ *    }
+ *    if(authorInp){
+ *        query.author=author
+ *    }
+ *    if(tags.length!=0)
+ *        query.tags=exact ? {$all:tags} : {$in:tags}
+ *    query=escapeForMongo(query)
+ *    let client, db
+ *    try{
+ *        client =await MongoClient.connect(url, {useNewUrlParser: true})
+ *        db = client.db("mydb")
+ *        let dCollection = db.collection('blogs')
+ *        const data = await dCollection.find(query).toArray()
+ *        return data
+ *    }
+ *    catch(err){ 
+ *        console.log(err)
+ *    }
+ *    finally{ 
+ *        client.close()
+ *    } 
+ *}
+ *
+ */
 http.listen(port, () => {
 	console.log(`listening on ${port}`)
 })
--- static/js/script.js
+++ static/js/script.js
@@ -13,6 +13,12 @@ let searchActive=false
 const blogs = []
 let ttlBlogs = 0;
 let len = 0
+const searchRet = {
+	ttl:0,
+	blogs:[],
+	len:0,
+	next:null
+}
 const closeIcon = `
 
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
@@ -48,163 +54,210 @@ const blog = ({title, body, author, tags , time, date}) => {
 					<div class="flex tags">
 						tags: 
 						${tags.map(tag=>{
-						return `<span>#${tag}</span>`
+							return `<span>#${tag}</span>`
 						}).join('')}
 					</div>
 				</div>
 			</div>
 	`
-}
-const getBlock = id => {
-	return document.getElementById(id)
-}
-const toggleElem = (id, elem=false) =>{
-	!elem ? getBlock(id).classList.toggle('hidden'):null
-	searchData[id]=!searchData[id]
-	updatePublished()
-}
-const clearTag = e => {
-	delete searchData.tags[e.id]
-	e.parentNode.remove()
-	updatePublished()
-}
-const toggleSearch = (e) => {
-	getBlock('search').classList.toggle('hidden')
-	searchActive=!searchActive
-	e.innerHTML=searchActive ? closeIcon : searchIcon
-}
-const updatePublished = () => {
-	if(searchData.dateInp || searchData.authorInp){
-		getBlock('publishData').innerHTML=`Published ${searchData.dateInp ? `in <span class="date" id="date">${searchData.date}</span>`:''} ${searchData.authorInp ? `by <span class="name" id="authorName">@${searchData.author}</span>`:''}`
-	}else{
-		getBlock('publishData').innerHTML=''
+						}
+	const getBlock = id => {
+		return document.getElementById(id)
 	}
-	if(Object.keys(searchData.tags).length==0){
-		getBlock("tags").classList.add('hidden')
-
-	}else{
-		getBlock("tags").classList.remove('hidden')
+	const toggleElem = (id, elem=false) =>{
+		!elem ? getBlock(id).classList.toggle('hidden'):null
+		searchData[id]=!searchData[id]
+		updatePublished()
 	}
-}
-//const data=(i) => {
-//return ({	title:"sample data "+i,
-	//body:"Elit distinctio doloremque cum vitae nihil Sed pariatur in eveniet quidem vero dolore Dolores voluptatum commodi rerum in esse. Incidunt vitae cupiditate quas expedita molestiae id aspernatur suscipit Facere nisi eveniet et vitae optio? Deserunt laudantium vitae ad sint laboriosam!",
-	//author:"anurag",
-	//tags:["coding", "hello"]
-//})
-//}
-
-//const addNew = async (i) => {
-	//await fetch('/add-new-post', {
-		//method: 'POST',
-		//headers: {
-			//'Accept': 'application/json, text/plain, */*',
-			//'Content-Type': 'application/json'
-		//},
-
-		//crossdomain: true,
-		//withCredentials:'include',
-		//body:JSON.stringify(data(i))
-	//})
-		//.then(response => response.json())
-		//.then(res=>console.log(res))	
-//}
-
-const search = async () => {
-	const data = JSON.parse(JSON.stringify(searchData))
-	data.tags=Object.keys(data.tags)
-	await fetch('/search', {
-		method: 'POST',
-		headers: {
-			'Accept': 'application/json, text/plain, */*',
-			'Content-Type': 'application/json'
-		},
-		crossdomain: true,
-		withCredentials:'include',
-		body:JSON.stringify({searchData:data })
-	})
-		.then(response => response.json())
-		.then(res=>{
-			setPosts(res)
-		})	
-}
-const getBlogs = async (fwd=true) => {
-	if(len>=ttlBlogs && ttlBlogs!=0){
-	getBlock('nextButton').classList.add('hidden')
-	return	
+	const clearTag = e => {
+		delete searchData.tags[e.id]
+		e.parentNode.remove()
+		updatePublished()
 	}
-	getBlock('loadMore').classList.toggle('hidden')
-	getBlock('spinner').classList.toggle('hidden')
-	await fetch('/blogs', {
-		method: 'POST',
-		headers: {
-			'Accept': 'application/json, text/plain, */*',
-			'Content-Type': 'application/json'
-		},
-		crossdomain: true,
-		withCredentials:'include',
-		body:JSON.stringify({next:(fwd?next:prev), fwd})
-	})
-		.then(response => response.json())
-		.then(res=>{
-			if(res.status){
-				next=res.next
-				getBlock('ttl').innerText=res.ttl
-				ttlBlogs=res.ttl
-				setPosts(res.items)
-			}})	
-	getBlock('loadMore').classList.toggle('hidden')
-	getBlock('spinner').classList.toggle('hidden')
-	
-}
+	const toggleSearch = (e) => {
+		getBlock('search').classList.toggle('hidden')
+		searchActive=!searchActive
+		e.innerHTML=searchActive ? closeIcon : searchIcon
+		main.innerHTML=''
+		if(searchActive){
+			searchRet.next=null
+		}else{
+			next=null
+			getBlogs(true)
+		}
+	}
+	const updatePublished = () => {
+		if(searchData.dateInp || searchData.authorInp){
+			getBlock('publishData').innerHTML=`Published ${searchData.dateInp ? `in <span class="date" id="date">${searchData.date}</span>`:''} ${searchData.authorInp ? `by <span class="name" id="authorName">@${searchData.author}</span>`:''}`
+		}else{
+			getBlock('publishData').innerHTML=''
+		}
+		if(Object.keys(searchData.tags).length==0){
+			getBlock("tags").classList.add('hidden')
 
-const setPosts=(items)=>{
-	items.map(item=>{
-		main.innerHTML+=blog(item)
-		blogs.push(item)
-		len++
-	})
-	getBlock('len').innerText=len
-}
+		}else{
+			getBlock("tags").classList.remove('hidden')
+		}
+	}
+	const data=(i) => {
+		return ({	title:"sample data "+i+100,
+			body:"Elit distinctio doloremque cum vitae nihil Sed pariatur in eveniet quidem vero dolore Dolores voluptatum commodi rerum in esse. Incidunt vitae cupiditate quas expedita molestiae id aspernatur suscipit Facere nisi eveniet et vitae optio? Deserunt laudantium vitae ad sint laboriosam!",
+			author:"shakti",
+			tags:["cat", "hello"]
+		})
+	}
 
-onload=()=>{
-//	addBlogs(10)
-//	for(let i = 0;i<100;i++)addNew(i+1)
-	getBlogs(true)
-	getBlock("inp").addEventListener('keydown', (e)=>{
-		if(e.keyCode == 13) {
-			e.preventDefault()
-			const tag = e.target.value
-			e.target.value=''
-			if(tag.length!=0 && tag!="" && tag!=null && tag!=undefined ){
-				getBlock('tags').innerHTML+=`<span  class="searchItem">#${tag}<span class="close" onclick="clearTag(this)" id=${tag}>x</span></span>` 
-				searchData.tags[tag]=tag
-				updatePublished()
+	const addNew = async (i) => {
+		await fetch('/add-new-post', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json, text/plain, */*',
+				'Content-Type': 'application/json'
+			},
+
+			crossdomain: true,
+			withCredentials:'include',
+			body:JSON.stringify(data(i))
+		})
+			.then(response => response.json())
+			.then(res=>console.log(res))	
+	}
+
+	const search = async (e) => {
+		len=0
+		getBlock('len')=0
+		if(searchRet.len>=searchRet.ttl && searchRet.ttl!=0){
+			getBlock('nextButton').classList.add('hidden')
+			return	
+		}
+		getBlock('loadMore').classList.toggle('hidden')
+		getBlock('spinner').classList.toggle('hidden')
+
+		const data = JSON.parse(JSON.stringify(searchData))
+		data.tags=Object.keys(data.tags)
+		await fetch('/search', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json, text/plain, */*',
+				'Content-Type': 'application/json'
+			},
+			crossdomain: true,
+			withCredentials:'include',
+			body:JSON.stringify({searchData:data, next:searchRet.next })
+		})
+			.then(response => response.json())
+			.then(res=>{
+				if(res.status){
+					if(searchRet.next===null){
+						main.innerHTML=''
+					}
+					searchRet.next=res.next
+					getBlock('ttl').innerText=res.ttl
+					searchRet.ttl=res.ttl
+					setPosts(res.items, true)
+				}
+			})	
+		getBlock('loadMore').classList.toggle('hidden')
+		getBlock('spinner').classList.toggle('hidden')
+
+	}
+	const getBlogs = async (fwd=true) => {
+		if(len>=ttlBlogs && ttlBlogs!=0){
+			getBlock('nextButton').classList.add('hidden')
+			return	
+		}
+		getBlock('loadMore').classList.toggle('hidden')
+		getBlock('spinner').classList.toggle('hidden')
+		await fetch('/blogs', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json, text/plain, */*',
+				'Content-Type': 'application/json'
+			},
+			crossdomain: true,
+			withCredentials:'include',
+			body:JSON.stringify({next:(fwd?next:prev), fwd})
+		})
+			.then(response => response.json())
+			.then(res=>{
+				if(res.status){
+					if(next===null){
+						main.innerHTML=''
+					}
+					next=res.next
+					getBlock('ttl').innerText=res.ttl
+					ttlBlogs=res.ttl
+					setPosts(res.items)
+				}})	
+		getBlock('loadMore').classList.toggle('hidden')
+		getBlock('spinner').classList.toggle('hidden')
+
+	}
+
+	const setPosts=(items, search=false)=>{
+		if(search){
+			items.map(item=>{
+				main.innerHTML+=blog(item)
+				searchRet.blogs.push(item)
+				searchRet.len++
+			})
+			getBlock('len').innerText=searchRet.len
+			return
+
+		}
+		items.map(item=>{
+			main.innerHTML+=blog(item)
+			blogs.push(item)
+			len++
+		})
+		getBlock('len').innerText=len
+	}
+
+	onload=()=>{
+		//	for(let i = 0;i<30;i++)addNew(i+1)
+		getBlogs(true)
+		getBlock("inp").addEventListener('keydown', (e)=>{
+			if(e.keyCode == 13) {
+				e.preventDefault()
+				const tag = e.target.value
+				e.target.value=''
+				if(tag.length!=0 && tag!="" && tag!=null && tag!=undefined ){
+					getBlock('tags').innerHTML+=`<span  class="searchItem">#${tag}<span class="close" onclick="clearTag(this)" id=${tag}>x</span></span>` 
+					searchData.tags[tag]=tag
+					updatePublished()
+				}
 			}
-		}
-	})
-	getBlock("authorInp").addEventListener('keydown', (e)=>{
-		if(e.keyCode == 13) {
-			e.preventDefault()
-			const tag = e.target.value
-			e.target.value=''
-			if(tag.length!=0 && tag!="" && tag!=null && tag!=undefined ){
-				searchData.author=tag
-				updatePublished()
-				getBlock('authorName').innerText=`@${tag}`
+		})
+		getBlock("authorInp").addEventListener('keydown', (e)=>{
+			if(e.keyCode == 13) {
+				e.preventDefault()
+				const tag = e.target.value
+				e.target.value=''
+				if(tag.length!=0 && tag!="" && tag!=null && tag!=undefined ){
+					searchData.author=tag
+					updatePublished()
+					getBlock('authorName').innerText=`@${tag}`
+				}
 			}
-		}
-	})
-	getBlock("dateInp").addEventListener('change', (e)=>{
-		e.preventDefault()
-		const val = e.target.value.split('-')
-		const tag = val[1]+'/'+val[0]
-		if(tag.length!=0 && tag!="" && tag!=null && tag!=undefined ){
-			searchData.date=tag
-			updatePublished()
-			getBlock('date').innerText=`${tag}`
-		}
-	})
+		})
+		getBlock("dateInp").addEventListener('change', (e)=>{
+			e.preventDefault()
+			const val = e.target.value.split('-')
+			const tag = val[1]+'/'+val[0]
+			if(tag.length!=0 && tag!="" && tag!=null && tag!=undefined ){
+				searchData.date=tag
+				updatePublished()
+				getBlock('date').innerText=`${tag}`
+			}
+		})
 
-	updatePublished()
-}
+		getBlock('nextButton').addEventListener('click', (e)=>{
+			if(!searchActive){
+				getBlogs(true)	
+			}else{
+				search()
+			}
+		})
+
+		updatePublished()
+	}
